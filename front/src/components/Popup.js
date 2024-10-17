fe0b8a4e26032f77081bc1ec393cdd86c24037ef
sucloudflare <cloudflare.ddos21@gmail.com>
2024-10-17 13:56:57 -0300

    Resolvendo conflitos

--- front/src/components/Popup.js
+++ front/src/components/Popup.js
@@ -4,10 +4,19 @@ import './styles.css'; // Importando o CSS
 
 const Popup = () => {
     return (
-        <div className="card">
-            <h1>Cashback Disponível!</h1>
-            <p>Visite nossas lojas parceiras e ganhe cashback nas suas compras.</p>
-            <Cashback />
+        <div className="popup-container">
+            <div className="popup-card">
+                <img 
+                    src="/mtrek.png" // Caminho correto para acessar a imagem na pasta public
+                    alt="Cashback"
+                    className="cashback-image" 
+                    width={40} // Remover 'px' e usar apenas o número
+                />
+                <h1>Cashback Disponível!</h1>
+                <p>Visite nossas lojas parceiras e ganhe cashback nas suas compras.</p>
+                <Cashback />
+                <button className="action-button">Ver Ofertas</button>
+            </div>
         </div>
     );
 };
